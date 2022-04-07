c284b1fd213b39a46afee9ac14b8222402fd4d65
matthias <meretzmatthias@yahoo.de>
2022-04-07 15:57:36 +0200

    Implement operate function

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b; 
+const divide = (a, b) => a / b;
+
+const operate = (operator, a, b) => {
+  if (operator === "+") return add(a, b)
+  if (operator === "-") return subtract(a, b)
+  if (operator === "*") return multiply(a, b)
+  if (operator === "/") return divide(a, b)
+}
